7c71a70fabf6323211fa7e231f59eac1b7a1a17d
Pratiksha Ravindra Vaidya <vaidyapratiksha259@gmail.com>
2026-07-18 22:32:15 +0530

    Added user input handling using Scanner in Java

---
+++ UserInputProgram.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class UserInputProgram {
+    public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.print("Enter your name: ");
+        String name = sc.nextLine();
+        
+        System.out.print("Enter your age: ");
+        int age = sc.nextInt();
+        
+        System.out.println("Hello " + name + ", you are " + age + " years old.");
+    }
+}
