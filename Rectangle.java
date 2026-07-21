41943b3d54cc0ac9760a2aeedc2d73957211dffb
Pratiksha Ravindra Vaidya <vaidyapratiksha259@gmail.com>
2026-07-21 21:08:52 +0530

    program to calculate the area of rectangle

---
+++ Rectangle.java
@@ -0,0 +1,12 @@
+import java.io.*;
+import java.lang.*;
+class Addition
+  {
+    public static void main(String arg[])
+    {
+      int l=5;
+      int b=5;
+      int area=l*b;
+      System.out.println("The Area of Rectangle is "+area);
+    }
+  }
