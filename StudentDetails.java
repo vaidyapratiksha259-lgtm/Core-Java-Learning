e831c183034e12cb472b1f0e11ad125db7d16325
Pratiksha Ravindra Vaidya <vaidyapratiksha259@gmail.com>
2026-07-21 20:49:30 +0530

    Added a program to print the details of student

---
+++ StudentDetails.java
@@ -0,0 +1,14 @@
+import java.io.*;
+import java.lang.*;
+class StudentDetails
+  {
+    public static void main(String args[])
+    {
+      System.out.pritnln("Name : Pratiksha Vaidya ");
+      System.out.pritnln("College : PRMIT&R ");
+      System.out.pritnln("Branch : CSE ");
+      System.out.pritnln("City : Badnera ");
+      System.out.pritnln("Year : 2028 ");
+    }
+  }
+      
