133750057d445792795e8fe45f6d8d157f520bdc
Pratiksha Ravindra Vaidya <vaidyapratiksha259@gmail.com>
2026-07-21 20:57:15 +0530

    Create Addition.java

---
+++ Addition.java
@@ -0,0 +1,12 @@
+import java.io.*;
+import java.lang.*;
+class Addition
+  {
+    public static void main(String args[])
+    {
+      int n1=10;
+      int n2=20;
+      int add=n1+n2;
+      System.out.println("Addition of "+n1+" and "+n2+" is "+add);
+    }
+  }
