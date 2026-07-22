3546a2325e2c0ca9262557b82505eeed171afdc6
Pratiksha Ravindra Vaidya <vaidyapratiksha259@gmail.com>
2026-07-22 22:00:23 +0530

    Program to print the details of student and calculate the percentage of student

---
+++ Percentage.java
@@ -0,0 +1,18 @@
+import java.io.*;
+import java.lang.*;
+class Percentage
+{
+public static void main(String args[])
+{
+int tm=445;
+int ofm=500;
+int rollno=15;
+String Name="Pratiksha Vaidya";
+System.out.println("Name : "+Name);
+System.out.println("Roll No. : "+rollno);
+System.out.println("Total Marks : "+tm);
+System.out.println("Out Of Marks : "+ofm);
+double per=(tm*100)/ofm;
+System.out.println("Percentage : "+per);
+}
+}
