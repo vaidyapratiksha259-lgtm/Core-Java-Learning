759a0a3722f169a5400283af6d0afaba22cf7ac5
Pratiksha Ravindra Vaidya <vaidyapratiksha259@gmail.com>
2026-07-23 08:58:34 +0530

    program to calculate a bill of customer

---
+++ Totalbill.java
@@ -0,0 +1,13 @@
+import java.io.*;
+import java.lang.*;
+class Totalbill
+  {
+    public static void main(String args[])
+    {
+      int product,purchaseamount;
+      product=5;
+      purchaseamount=600;
+      double bill=product*purchaseamount;
+      System.out.println("TotalBill : "+bill);
+    }
+  }
