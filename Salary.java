61aef03de6602e478151dcdeb5aba30d0c2af1a2
Pratiksha Ravindra Vaidya <vaidyapratiksha259@gmail.com>
2026-07-23 08:37:27 +0530

    program to calculate salary of worker by using a wadges and working days

---
+++ Salary.java
@@ -0,0 +1,12 @@
+import java.io.*;
+import java.lang.*;
+class Salary
+  {
+    public static void main(String args[])
+    {
+      int wadges=100;
+      int Workingdays=10;
+      int salary=wadges*Workingdays;
+      System.out.println("Salary : "+salary);
+    }
+  }
