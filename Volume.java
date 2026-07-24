17e8b2b6f87f9e14d69effce12e1bb4037b1bf4f
Pratiksha Ravindra Vaidya <vaidyapratiksha259@gmail.com>
2026-07-24 22:13:01 +0530

    program to calculate the volume of sphere

--- Volume.java
+++ Volume.java
@@ -7,7 +7,7 @@ class Volume
     {
         float r = 2.5f, vol;
 
-        vol = (4 * 3.14f * r * r * r) / 3;
+        vol = (4 * 3.142f * r * r ) / 3;
 
         System.out.print("The volume of sphere is " + vol);
     }
