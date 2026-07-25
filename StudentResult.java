ec5737c84d91c3470af23ff4b6d86e60c6fea926
Pratiksha Ravindra Vaidya <vaidyapratiksha259@gmail.com>
2026-07-25 12:39:36 +0530

    program to calculate a student result i.e pass or fail by using a ternary operator

---
+++ StudentResult.java
@@ -0,0 +1,12 @@
+import java.io.*;
+import java.lang.*;
+class StudentResult
+  {
+    public static void main(String args[])
+    {
+      int marks=25;
+      String z;
+      z=marks>=35?"Pass":"Fail";
+      System.out.println("Student is "+z+" in Subject");
+    }
+  }
