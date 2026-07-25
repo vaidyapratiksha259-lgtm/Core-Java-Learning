56c3a6194a9553857e0783c53c0f394008ef6eb1
Pratiksha Ravindra Vaidya <vaidyapratiksha259@gmail.com>
2026-07-25 12:47:55 +0530

    program to check the eligibility of voter by using a ternary operator

---
+++ Voter.java
@@ -0,0 +1,13 @@
+import java.io.*;
+import java.lang.*;
+class Voter
+  {
+    public static void main(String args[])
+    {
+      int age=19;
+      String name,z;
+      name="Pratiksha";
+      z=age>=18?"Eligible to vote":"Not Eligible to vote";
+      System.out.println("name is "+z);
+    }
+  }
