82c19bfa5c2c9daf16c195a0b219052bcea101ee
Pratiksha Ravindra Vaidya <vaidyapratiksha259@gmail.com>
2026-07-26 12:39:02 +0530

    program to initialize a two number and perform the all Arithmetic operation on it

---
+++ ArithmeticOpeartion.java
@@ -0,0 +1,24 @@
+import java.io.*;
+import java.lang.*;
+class ArithmeticOpeartion
+{
+public static void main(String args[])
+{
+int a,b;
+a=10;
+b=5;
+int add,sub,div,mul,mod;
+add=a+b;
+sub=a-b;
+mul=a*b;
+mod=a%b;
+div=a/b;
+System.out.println("Addition :"+add);
+System.out.println("Substraction :"+sub);
+System.out.println("Multiplication :"+mul);
+System.out.println("Division :"+div);
+System.out.println("Modulo :"+mod);
+}
+}
+
+
