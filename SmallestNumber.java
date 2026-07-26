944bcf8d768ea91e112cfdbc28a217524e94f944
Pratiksha Ravindra Vaidya <vaidyapratiksha259@gmail.com>
2026-07-26 12:34:59 +0530

    program to find the smallest number by using a Three way operator

---
+++ SmallestNumber.java
@@ -0,0 +1,13 @@
+import java.io.*;
+import java.lang.*;
+class SmallestNumber
+  {
+    public static void main(String args[])
+    {
+      int x,y;
+      x=10;
+      y=25;
+      String z=x<y?"x is Smallest":"y is Smallest";
+      System.out.println(z);
+}
+}
