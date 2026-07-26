21d259c4c6d94f82f6ba10389fcaea5f2b30e02a
Pratiksha Ravindra Vaidya <vaidyapratiksha259@gmail.com>
2026-07-26 12:32:21 +0530

    program to find the greatest number by using a conditional operator

---
+++ GreatestNumber.java
@@ -0,0 +1,13 @@
+import java.io.*;
+import java.lang.*;
+class GreatestNumber
+  {
+    public static void main(String args[])
+    {
+      int x,y;
+      x=10;
+      y=25;
+      String z=x>y?"x is Greater":"y is Greater";
+      System.out.println(z);
+}
+}
