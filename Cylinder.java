e6afb58e0ddff7be5c270d3137cc23c4f2fc7a18
Pratiksha Ravindra Vaidya <vaidyapratiksha259@gmail.com>
2026-07-27 22:55:30 +0530

    program to calculate the area of cylinder

---
+++ Cylinder.java
@@ -0,0 +1,11 @@
+import java.io.*;
+import java.lang.*;
+class Cylinder
+  {
+    public static void main(String args[])
+    {
+      double r=10.5,h=20.5;
+      double area=3.142*r*r*h;
+      System.out.println("The Area Of Cylinder is : "+area);
+    }
+  }
