f4f10cd8e87d189603cc3537ebd03476c7a87581
Pratiksha Ravindra Vaidya <vaidyapratiksha259@gmail.com>
2026-07-27 22:50:27 +0530

    program to find the given no is even or odd by using a three way operator

---
+++ EvenOdd.java
@@ -0,0 +1,11 @@
+import java.io.*;
+import java,lang.*;
+class Even
+  {
+    public static void main(String args[])
+    {
+      int n=10;
+      String z=n>0?"Number is Even":"Number is Odd";
+      System.out.println(z);
+    }
+  }
