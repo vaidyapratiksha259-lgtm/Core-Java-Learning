86772a7d02454bdc53a905a755babaa8210e8034
Pratiksha Ravindra Vaidya <vaidyapratiksha259@gmail.com>
2026-07-28 22:09:04 +0530

    program to check the given number is positive number or negative number

---
+++ CheckNumber.java
@@ -0,0 +1,11 @@
+import java.io.*;
+import java.lang.*;
+class CheckNumber
+  {
+    public static void main(String args[])
+    {
+      int n=25;
+      String z=n>0?"The Given Number is Positive Number":"The Given Number is Negative Number";
+      System.out.println(z);
+    }
+  }
