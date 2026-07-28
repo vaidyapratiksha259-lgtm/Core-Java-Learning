dffe8f9de2e91627b42d680a85bdb9bfe8342934
Pratiksha Ravindra Vaidya <vaidyapratiksha259@gmail.com>
2026-07-28 22:12:09 +0530

    program to check the Person is Eligible to vote or not

---
+++ BloodDonation.java
@@ -0,0 +1,11 @@
+import java.io.*;
+import java.lang.*;
+class BloodDonation
+  {
+    public static void main(String args[])
+    {
+      int n=19;
+      String z=n>=18?"Person is Eligible to vote":"Person is not Eligible to vote";
+      System.out.println(z);
+    }
+  }
