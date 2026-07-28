0b0fdb97ba220d2315f21b39530a61b34e5600f3
Pratiksha Ravindra Vaidya <vaidyapratiksha259@gmail.com>
2026-07-28 22:18:07 +0530

    program to check the eligibility of Blood Donation on the basis of two parameter that is age and weight

---
+++ Donar.java
@@ -0,0 +1,13 @@
+import java.io.*;
+import java.lang.*;
+class Donar
+  {
+    public static void main(String args[])
+    {
+     double age,weight;
+      age=22;
+      weight=45;
+      String z=age>=18 && weight>45?" Eligible For Blood Donation ":"Not Eligible For Blood Donation";
+      System.out.println(z);
+    }
+  }
