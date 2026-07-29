05a4af2be148bdb08913fe15ee100c122c6cd458
Pratiksha Ravindra Vaidya <vaidyapratiksha259@gmail.com>
2026-07-29 08:41:11 +0530

    program to calculate a bonus of employee which is 20% of salary

---
+++ EmployeeBonus.java
@@ -0,0 +1,20 @@
+import java.io.*;
+import java.lang.*;
+class EmployeeBonus
+  {
+    public static void main(String args[])
+    {
+      String name,email;
+      double salary,bonus;
+      name="Pratiksha Vaidya";
+      email="pratiksha@gmail.com";
+      salary=50000.0d;
+bonus=salary*0.20;
+      System.out.println("***Employee Details***");
+      System.out.println("Name = "+name);
+      System.out.println("Email = "+email);
+      System.out.println("Salary = "+salary);
+      System.out.println("Bouns = "+bonus);
+
+    }
+  }
