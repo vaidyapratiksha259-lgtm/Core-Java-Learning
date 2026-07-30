8141be31ae9bb5218653dd807c11fd29f0d8ce03
Pratiksha Ravindra Vaidya <vaidyapratiksha259@gmail.com>
2026-07-30 11:25:54 +0530

    program to take a height and base as a input from the user and calculate the area  of triangle

---
+++ Area.java
@@ -0,0 +1,15 @@
+import java.util.*;
+class Area
+{
+public static void main(String args[])
+{
+Scanner sc=new Scanner(System.in);
+System.out.print("Enter Height : ");
+int h=sc.nextInt();
+System.out.print("Enter Base : ");
+int b=sc.nextInt();
+int area=(b*h)/2;
+System.out.println("The Area is = "+area);
+}
+}
+
