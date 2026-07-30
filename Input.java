47c35eba160f972d2d8af2faca4f129122f3e236
Pratiksha Ravindra Vaidya <vaidyapratiksha259@gmail.com>
2026-07-30 11:23:53 +0530

    program to take the input from user

---
+++ Input.java
@@ -0,0 +1,15 @@
+import java.io.*;
+import java.util.*;
+class Input
+{
+public static void main(String args[])
+{
+Scanner sc=new Scanner(System.in);
+System.out.print("Enter a First Number : ");
+int n1=sc.nextInt();
+System.out.print("Enter a Second Number : ");
+int n2=sc.nextInt();
+int add=n1+n2;
+System.out.println("The Addition Of "+ n1 +" And "+ n2 +"Number is = "+add);
+}
+}
