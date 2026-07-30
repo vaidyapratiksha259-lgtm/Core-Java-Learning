6caf47c6a8bc4b0d46cf6c04f85ea220a10d4646
Pratiksha Ravindra Vaidya <vaidyapratiksha259@gmail.com>
2026-07-30 11:22:23 +0530

    program to calculate a milage of car

---
+++ Milage.java
@@ -0,0 +1,18 @@
+import java.io.*;
+import java.lang.*;
+class Milage
+{
+public static void main(String args[])
+{
+String modelname="Maruti Suzuki";
+int distance,fuel;
+distance=700;
+fuel=60;
+float milage=distance/fuel;
+System.out.println("***Car Details***");
+System.out.println("Model Name = "+modelname);
+System.out.println("Distance = "+distance);
+System.out.println("Fuel= "+fuel);
+System.out.println("Milage= "+milage);
+}
+}
