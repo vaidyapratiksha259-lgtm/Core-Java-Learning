d11e7a2481f945bc113a2bc76c15cc602523b0b1
Pratiksha Ravindra Vaidya <vaidyapratiksha259@gmail.com>
2026-08-02 21:39:44 +0530

    Added java program to check student pass or fail using if-else statement

---
+++ Student.java
@@ -0,0 +1,19 @@
+import java.util.*;
+class Marks
+  {
+    public static void main(String args[])
+    {
+      Scanner sc=new Scanner(System.in);
+      System.out.print("Enter Marks of Student : ");
+      int marks=sc.nextInt();
+      if(marks>=35)
+      {
+        System.out.print("Student is pass");
+      }
+      else
+      {
+        System.out.print("Student is fail");
+      }
+
+    }
+}
