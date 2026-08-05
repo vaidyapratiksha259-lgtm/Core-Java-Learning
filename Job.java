6aa832fa7fe2e656c4660c1d73d307daf2e01c22
Pratiksha Ravindra Vaidya <vaidyapratiksha259@gmail.com>
2026-08-05 12:53:19 +0530

    program to check the person is eligible for job or not by using a if-else statement

---
+++ Job.java
@@ -0,0 +1,29 @@
+import java.util.*;
+class Job
+  {
+    public static void main(String args[])
+    {
+      Scanner sc=new Scanner(System.in);
+      System.out.print("Enter Age : ");
+      int age=sc.nextInt();
+      System.out.print("Enter Experience : ");
+      double exp=sc.nextDouble();
+
+    if(age>=25 & age<=25 && exp>=3)
+    {
+      System.out.println("Person is Eligible for job ");
+    }
+      else
+    {
+    System.out.println("Person is not Eligible for job ");
+    }
+    }
+  }
+
+
+
+
+
+
+
+      
