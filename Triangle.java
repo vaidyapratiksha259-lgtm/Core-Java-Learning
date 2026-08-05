40b698791f9a6959e6786332afb3220ca8b6e298
Pratiksha Ravindra Vaidya <vaidyapratiksha259@gmail.com>
2026-08-05 12:58:33 +0530

    program to check whether the enter angle is satisfy the triangle or not

---
+++ Triangle.java
@@ -0,0 +1,33 @@
+import java.util.*;
+
+class Triangle
+  {
+    public static void main(String args[])
+    {
+      Scanner sc=new Scanner(System.in);
+
+      System.out.print("Enter Aangle a : ");
+      int a=sc.nextInt();
+      System.out.print("Enter Aangle b : ");
+      int b=sc.nextInt();
+      System.out.print("Enter Aangle c : ");
+      int c=sc.nextInt();
+
+    if(a+b+c==180)
+    {
+      System.out.println("The Angle is satisfy the triangle ");
+    }
+      else
+    {
+    System.out.println("The Angle is not satisfy the triangle ");
+    }
+    }
+  }
+
+
+
+
+
+
+
+      
