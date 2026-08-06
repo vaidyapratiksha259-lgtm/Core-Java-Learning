8c6afb9399e7b372d102ffc5db64b9ea78bdda87
Pratiksha Ravindra Vaidya <vaidyapratiksha259@gmail.com>
2026-08-06 09:43:42 +0530

    Added a program to check a enter number is two digit or not

---
+++ Numbertwodigit.java
@@ -0,0 +1,30 @@
+import java.io.*;
+import java.lang.*;
+import java.util.*;
+class Numbertwodigit
+  {
+    public static void main(String args[])
+    {
+      Scanner sc=new Scanner(System.in);
+      System.out.print("Enter a Number: ");
+      int num=sc.nextInt();
+
+          if(num>=10 && num<=99)
+    {
+            System.out.println("Enter Number is two Digit Number");
+    }
+      else
+    {
+            System.out.println("Enter Number is Not two Digit Number");
+
+    }
+    }
+  }
+
+
+
+
+
+
+
+      
