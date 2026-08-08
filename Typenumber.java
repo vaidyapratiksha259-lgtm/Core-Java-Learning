d37657cf31b5d3d9a5956cff435c063e686bd749
Pratiksha Ravindra Vaidya <vaidyapratiksha259@gmail.com>
2026-08-08 21:21:02 +0530

    added a program to check the type of number that is positive negative or zero

---
+++ Typenumber.java
@@ -0,0 +1,27 @@
+import java.io.*;
+import java.lang.*;
+import java.util.*;
+class Typenumber
+  {
+    public static void main(String args[])
+    {
+      Scanner sc=new Scanner(System.in);
+      System.out.print("Enter a Number : ");
+      int num=sc.nextInt();
+      if(num>0)
+      {
+              System.out.println("Enetr Number is Positive ");
+      }
+      else if(num<0)
+      {
+              System.out.println("Enetr Number is Negative ");
+      }
+      else
+      {
+          System.out.println("Enetr Number is Zero ");
+      }
+    }
+    }
+
+      
+        
