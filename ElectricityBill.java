56bc0232372e89b5435613eeea89e362ee91124d
Pratiksha Ravindra Vaidya <vaidyapratiksha259@gmail.com>
2026-08-15 23:15:58 +0530

    Update ElectricityBill.java

--- ElectricityBill.java
+++ ElectricityBill.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class ElectricityBill 
+public class ElectricityBill {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
