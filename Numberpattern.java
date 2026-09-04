366b2adfddce18b4e22c00e123d55b46caae9628
Pratiksha Ravindra Vaidya <vaidyapratiksha259@gmail.com>
2026-09-04 21:56:58 +0530

    Added number pyramid pattern program

---
+++ Numberpattern.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class NumberPyramid {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter number of rows: ");
+        int n = sc.nextInt();
+
+        for (int i = 1; i <= n; i++) {
+
+            // Spaces
+            for (int j = i; j < n; j++) {
+                System.out.print(" ");
+            }
+
+            // Numbers
+            for (int j = 1; j <= i; j++) {
+                System.out.print(j + " ");
+            }
+
+            System.out.println();
+        }
+
+        sc.close();
+    }
+}
