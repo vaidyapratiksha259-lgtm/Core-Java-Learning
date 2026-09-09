4c1f1506b5d4d8099202a5bd808bbf861d122c68
Pratiksha Ravindra Vaidya <vaidyapratiksha259@gmail.com>
2026-09-09 23:38:47 +0530

    Create diamond number pattern

---
+++ Diamondpattern.java
@@ -0,0 +1,34 @@
+public class DiamondPattern {
+    public static void main(String[] args) {
+
+        int n = 5;
+
+        // Upper half
+        for (int i = 1; i <= n; i++) {
+
+            for (int j = i; j < n; j++) {
+                System.out.print(" ");
+            }
+
+            for (int j = 1; j <= (2 * i - 1); j++) {
+                System.out.print(j);
+            }
+
+            System.out.println();
+        }
+
+        // Lower half
+        for (int i = n - 1; i >= 1; i--) {
+
+            for (int j = i; j < n; j++) {
+                System.out.print(" ");
+            }
+
+            for (int j = 1; j <= (2 * i - 1); j++) {
+                System.out.print(j);
+            }
+
+            System.out.println();
+        }
+    }
+}
